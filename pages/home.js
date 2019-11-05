ba8615339cdc393343b986701da1907635ba7589
Andre Rego <andremregodev@gmail.com>
2019-11-04 23:20:29 -0300

    Servidor Customizado, estilização, root import

--- pages/home.js
+++ pages/home.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Link from 'next/link';
 import Head from 'next/head';
 
-import withAnalytics from '../src/hocs/withAnalytics';
+import withAnalytics from '~/hocs/withAnalytics';
+
+const Title = styled.h1`
+  color: #069;
+  font-size: 40px;
+`;
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
     </Head>
-    <h1>Hello</h1>
+    <img src="/static/panda.jpg" width="200" />
+    <Title>Hello</Title>
     <Link href="/users">
       <a>Usuários</a>
     </Link>
